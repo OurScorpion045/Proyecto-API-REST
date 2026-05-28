1be2fabd3a74c7fef175e57047add34a3b886669
Mario Alejandro Hdez Lopez <sams24484@gmail.com>
2026-05-28 16:33:03 -0700

    Avance: Archivo UsuarioController.php

--- src/controllers/UsuarioController.php
+++ src/controllers/UsuarioController.php
@@ -3,9 +3,31 @@
 
     use src\models\UsuarioModel;
     use src\config\Database;
+    use PDO;
+    use PDOException;
 
     class UsuarioController {
         private $database;
         private $connection;
+
+        function __construct() {
+            $this->database = new Database();
+            $this->connection = $this->database->getConnection();
+        }
+
+        public function getAllUsuarios() {
+            $sql = "SELECT * FROM usuarios ORDER BY usuarioId DESC";
+            $stmt = $this->connection->prepare($sql);
+            $stmt->execute();
+            return $stmt->fetchAll();
+        }
+
+        public function getUsuariosById($id) {
+            $sql = "SELECT * FROM usuarios WHERE id = :id";
+            $stmt = $this->connection->prepare($sql);
+            $stmt->bindValue(":id", $id, PDO::PARAM_INT);
+            $stmt->execute();
+            return $stmt->fetch();
+        }
     }
 ?>
