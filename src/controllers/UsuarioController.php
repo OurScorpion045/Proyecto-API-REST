aceb94496384dae21e7c289acae66af0259b653d
Mario Alejandro Hdez Lopez <sams24484@gmail.com>
2026-05-28 12:22:30 -0700

    Correccion de autoload y namespace: Archivo UsuarioController.php

---
+++ src/controllers/UsuarioController.php
@@ -0,0 +1,11 @@
+<?php
+    namespace src\controllers;
+
+    use src\models\UsuarioModel;
+    use src\config\Database;
+
+    class UsuarioController {
+        private $database;
+        private $connection;
+    }
+?>
