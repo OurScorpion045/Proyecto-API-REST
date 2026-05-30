4951fb53e8505b6d14093d1e0f54efddd358ab6d
Mario Alejandro Hdez Lopez <sams24484@gmail.com>
2026-05-29 23:12:02 -0700

    Avance: Archivo index.php

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
+    header("Content-type: application/json");
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+    header("Access-Control-Allow-Headers: Content-Type");
+
+    if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+        http_response_code(200);
+        exit();
+    }
+
     require_once "./autoload.php";
 ?>
