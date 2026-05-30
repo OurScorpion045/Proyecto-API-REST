77d409539dceb9a15cefabb163043e891528e039
Mario Alejandro Hdez Lopez <sams24484@gmail.com>
2026-05-30 00:02:50 -0700

    Correccion: Archivo index.php

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
     header("Content-type: application/json");
     header("Access-Control-Allow-Origin: *");
     header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
@@ -8,7 +12,7 @@
         http_response_code(200);
         exit();
     }
-    
+
     require_once "./autoload.php";
-    
+    require_once "./src/routes/UsuarioRouter.php";
 ?>
