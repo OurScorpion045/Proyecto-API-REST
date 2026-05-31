5ac7921d132ab3e8d402b50f8b87fe17eeec990c
Mario Alejandro Hdez Lopez <sams24484@gmail.com>
2026-05-31 07:57:38 -0700

    Avance: Archivo PacienteRouter.php

--- src/routes/PacienteRouter.php
+++ src/routes/PacienteRouter.php
@@ -28,7 +28,53 @@
                 }
                 break;
             case "POST":
-                
+                $data = json_decode(file_get_contents("php://input"), true);
+                $controller->insertPaciente(
+                    $data["DNI"],
+                    $data["Nombre"],
+                    $data["Direccion"],
+                    $data["CodigoPostal"],
+                    $data["Telefono"],
+                    $data["Genero"],
+                    $data["FechaNacimiento"],
+                    $data["Correo"]
+                );
+                break;
+            case "PUT":
+                $data = json_decode(file_get_contents("php://input"), true);
+
+                if (!$id) {
+                    http_response_code(404);
+                    echo json_encode(["error" => "Id no encontrado"]);
+                    exit;
+                }
+
+                $controller->updatePaciente(
+                    $id,
+                    $data["DNI"],
+                    $data["Nombre"],
+                    $data["Direccion"],
+                    $data["CodigoPostal"],
+                    $data["Telefono"],
+                    $data["Genero"],
+                    $data["FechaNacimiento"],
+                    $data["Correo"]
+                );
+                break;
+            case "DELETE":
+                if (!$id) {
+                    http_response_code(404);
+                    echo json_encode(["error" => "Id no encontrado"]);
+                    exit;
+                }
+                $controller->deletePaciente($id);
+                break;
+            default:
+                http_response_code(400);
+                echo json_encode(["error" => "Metodo no permitido"]);
         }
+    } else {
+        http_response_code(404);
+        echo json_encode(["error" => "Ruta no encontrada"]);
     }
 ?>
