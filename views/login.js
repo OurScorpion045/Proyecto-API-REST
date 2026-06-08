93ff20dda682c6cf956c0f4b78957cb6f37cd231
Mario Alejandro Hdez Lopez <sams24484@gmail.com>
2026-06-07 18:17:55 -0700

    Avance: Archivo login.js

---
+++ views/login.js
@@ -0,0 +1,10 @@
+
+
+const usuario = document.getElementById("usuario");
+const password = document.getElementById("password");
+const submit = document.getElementById("submit");
+const cancelar = document.getElementById("cancelar");
+
+cancelar.addEventListener("click", () => {
+    window.location.href = "../index.html";
+})
